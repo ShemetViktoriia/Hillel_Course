d823b2499fa4ee95770110d0a2c68a35fa2a7b5c
Viktoriia Shemet <shemet.felicity@gmail.com>
2023-09-05 00:31:07 +0300

    Add projects for the Lesson 3

---
+++ Lesson_3/Constants/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Constants
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double PI = Math.PI; 
+
+            // const double EXP; ERROR
+
+            // PI = 3.14; // ERROR
+
+            var variable = (5 + 2) / 2;
+
+            int a = 5;
+            char c = 'a';
+            string s = "test";
+            // null 
+
+            string t = null;
+
+            //var var2 = null; ERROR
+
+
+            int num1 = 300;
+            byte num2 = (byte)num1;
+
+            Console.ReadKey();
+        }
+
+        // XML comments
+        /// <summary>
+        /// Test description
+        /// </summary>
+        /// <param name="a">Test param</param>
+        /// <returns>Test return value</returns>
+        static int Print(int a)
+        {
+            Console.WriteLine("Test");
+            return a;
+        }
+    }
+}
