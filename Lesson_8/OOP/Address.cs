8df4976f62148f4de89986f7c53ebd68962fff53
Viktoriia Shemet <shemet.felicity@gmail.com>
2023-09-23 21:22:18 +0300

    Add Lesson_8

---
+++ Lesson_8/OOP/Address.cs
@@ -0,0 +1,26 @@
+﻿using System.Net;
+
+namespace OOP
+{
+    internal class Address
+    {
+		private int index; // 1111 - 9999
+
+		public int Index
+		{
+			get { return index; }
+			set 
+			{
+				if (value >= 1111 && value <= 9999)
+				{
+					index = value;
+				}
+			}
+		}
+
+		public void PrintAddress()
+		{
+            Console.WriteLine(Index);
+        }
+	}
+}
---
+++ Lesson_8/OOP/MyClass2.cs
@@ -0,0 +1,15 @@
+﻿namespace OOP
+{
+    public class MyClass2
+    {
+        // поля описують стан обєкта
+        /*private*/ int myValue; // default modificator  = private
+        public int myValuePublic;
+
+        // методи описують поведінку обєкта
+        public int GetValue()
+        {
+            return myValue;
+        }
+    }
+}
